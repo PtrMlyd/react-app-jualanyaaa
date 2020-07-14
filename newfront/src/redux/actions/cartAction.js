f7044c41e706f42657e514a52eaec1c280cdab92
ptrmlyd <triputramulyadi@gmail.com>
2020-07-14 19:46:20 +0700

    after error

--- newfront/src/redux/actions/cartAction.js
+++ newfront/src/redux/actions/cartAction.js
@@ -2,40 +2,39 @@
 import { ADD_TO_CART, REMOVE_FROM_CART } from "../types";
 
 //2. cart redux -  create addToCart yang menerima 2 parameter : 1. untuk  panggil item yang ada dialam cart , 2. product yang akan di passing ke cart and remove cart funtion
-export const addToCart = ( items, product ) => ( dispatch ) => {
+export const addToCart = ( product ) => ( dispatch, getState ) => {
     //3. cart redux - didalam cart, kita clone item dengan define item
-    const cart = items.slice();
+    const cartItems = getState().cart.cartItems.slice();
     // 4.cart redux -kita cari item yang akan di cek apakah product yang kita masukkab itu benar apa tidak, or pindahkan fungsi addto cart dan remove  yang di app.js 
     let alreadyExist = false
-    cart.forEach( x => {
+    cartItems.forEach( ( x ) => {
         if( x._id === product._id ) {
             alreadyExist = true ;
             x.count++;
         }
     });
     if( !alreadyExist ) {
-        cart.push( { ...product, count : 1 })
+        cartItems.push( { ...product, count : 1 })
     }
     dispatch( {
         type : ADD_TO_CART,
-        payload : { cart }
+        payload : { cartItems }
     })
-    localStorage.setItem('cartItems', JSON.stringify(cart) )
+    localStorage.setItem('cartItems', JSON.stringify( cartItems ) )
 }
 
-export const removeFromCart = (items, product) => (dispatch) => {
-    const cart = 
-        items
-        .slice()
+export const removeFromCart = ( product ) => ( dispatch, getState ) => {
+    const cartItems = getState()
+        .cart.cartItems.slice()
         .filter(
-            x  => x._id !== product._id 
+           ( x ) => x._id !== product._id 
         )
 
     dispatch({
         type : REMOVE_FROM_CART,
-        payload : { cart }
+        payload : { cartItems }
     })
     localStorage.setItem(
         'cartItems',
-        JSON.stringify( cart ) )
+        JSON.stringify( cartItems ) )
 }
--- newfront/src/redux/reducers/cartReducer.js
+++ newfront/src/redux/reducers/cartReducer.js
@@ -1,16 +1,16 @@
 import { ADD_TO_CART, REMOVE_FROM_CART } from "../types";
 
 export const cartReducer = ( 
-    state = { cart : JSON.parse(localStorage.getItem('cartItems')) || '[]' }
+    state = { cartItems : JSON.parse(localStorage.getItem('cartItems')) || '[]' }
     , action ) => {
     switch (action.type) {
         case ADD_TO_CART:
             return {
-                cart : action.payload.cart
+                cartItems : action.payload.cartItems
             }
         case REMOVE_FROM_CART :
             return  {
-                cart : action.payload.cart
+                cartItems : action.payload.cartItems
             }
         default:
             return state
