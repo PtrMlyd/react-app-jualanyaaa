c1624b1737ddc5d987d8a75cabd84849a2aa1d63
ptrmlyd <triputramulyadi@gmail.com>
2020-07-17 10:48:01 +0700

    ini baru

--- frontend/src/screens/user/ShoppingCartScreen.js
+++ frontend/src/screens/user/ShoppingCartScreen.js
@@ -28,79 +28,18 @@ function ShoppingCartScreen (props) {
     }, [])
 
     return <div className="cart">
-        {/* <div className='cart-list'>
-            <ul className="cart-list-container">
-                <li>
-                    <h3>
-                        Shopping Cart
-                    </h3>                   
-                    <div>
-                        Price
-                    </div>
-                </li>
-                {
-                    cartItems.length === 0 ?
-                    <div>
-                        Cart is Empty
-                    </div>
-                    :
-                    cartItems.map( item => 
-                        <li key={item.id}>
-                            <div className="cart-image" >
-                                <img src={item.image} alt={item.name} />
-                            </div>
-                            <div className="cart-name">
-                                <div>
-                                    <Link to={`/product/${item.id}`} >
-                                    {   item.name}
-                                    </Link> 
-                                </div>
-                                <div>
-                                    Qty:
-                                    <select value={item.qty} onChange={ (e) => dispatch(addToCart(item.id, e.target.value)) }>
-                                        {[...Array(item.inStock).keys()].map( x => 
-                                        <option key={ x + 1 } value={ x + 1 }> { x + 1} </option>
-                                        
-                                             )}
-                                    </select>
-                                    <button onClick={ () => deleteItem(item.id)} className="button" >
-                                        Delete
-                                    </button>
-                                </div>
-                            </div>
-                            <div className="cart-price">
-                                IDR {item.price}
-                            </div>
-                        </li>
-                    )
-                }
-            </ul>
-        </div>
-        <div className="cart-action">
-            <div >
-             <input type='text'  placeholder="YOUR VOUCHER CODE"></input>
-                <h3>
-                Subtotal 
-                :
-                IDR {cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
-                </h3>
-                <button onClick={checkOutItem} className='button primary full-width' disabled ={ cartItems.length === 0}>
-                    Proceed to Check Out
-                </button>
-            </div>
-        </div> */}
 
-        <table className='cart-list'>
+        <table className='table'>
             <thead >
-                <tr className="cart-header">
-                    <th className='cart-prod'>PRODUCT</th>
+                <tr>
+                    <th className='cart-head'>PRODUCT</th>
                     <th>PRICE</th>
                     <th>QUANTITY</th>
                     <th>TOTAL</th>
                     <th>ACTION</th>
                 </tr>
             </thead>
-            <tbody className="cart-body">
+            <tbody>
                 {
                     cartItems.length === 0 ?
                     <div>
@@ -108,7 +47,7 @@ function ShoppingCartScreen (props) {
                     </div>
                     :
                     cartItems.map( item => 
-                        <tr key={item.id} className='cart-key' >
+                        <tr key={item.id} >
                             <td className="cart-na">
                                 <div className="cart-image"> 
                                     <img src={item.image} alt={item.name} />
@@ -124,41 +63,39 @@ function ShoppingCartScreen (props) {
                                 <select value={item.qty} onChange={ (e) => dispatch(addToCart(item.id, e.target.value)) }>
                                     {[...Array(item.inStock).keys()].map( x => 
                                     <option key={ x + 1 } value={ x + 1 }> { x + 1} </option>
-                                    
-                                            )}
+                                    )}
                                 </select>
                             </td>
-                            <td> {cartItems.reduce(( c ) => c.price * c.qty, 0)} </td>
+                            <td> {item.price * item.qty} </td>
                             <td>
                                 <button onCtbodyck={ () => deleteItem(item.id)} className="button" >
                                     Delete
                                 </button>
                             </td>
                         </tr>
-
                     )
                 }              
-                
-                <tr className="cart-footer">
-                    <td>
-                        <div>
-                            <button>
-                                Back To Shop
-                            </button> 
-                        </div>
-                        <div>
-                            <button onClick={checkOutItem}>
-                                Proceed To Checkout
-                            </button>
-                        </div>
-                    </td>
-                    <td>
-                        <div> Sub Totoal </div> 
-                        <div> {cartItems.reduce((a, c) => a + c.price * c.qty, 0)} </div> 
-                    </td>
-                </tr>   
             </tbody>
         </table>
+        <div className='cartfoot'>
+            <div className='cartfootbut'>
+                <Link to='/shop'>
+                    <button className='button secondary '>
+                        Back to Shop
+                    </button>
+                </Link>
+                <button onClick={checkOutItem} className='button secondary' disabled ={ cartItems.length === 0}>
+                    Proceed to Check Out
+                </button>
+            </div>
+            <div >
+                <h3>
+                    Subtotal 
+                    :
+                    IDR {cartItems.reduce((a, c) => a + c.price * c.qty, 0)}
+                </h3>
+            </div>
+        </div>
     </div>
     
 }
